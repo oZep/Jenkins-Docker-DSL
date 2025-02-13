3179e099641fa05ee6c8b5251a1053ff6817104b
Joey Issa <joeyissa59@gmail.com>
2025-02-13 11:56:45 -0500

    jenshins tag

--- job.groovy
+++ job.groovy
@@ -8,6 +8,7 @@ job('Jenskins-Docker-DSL') {
     }
     steps {
         shell('''
+        git tag -a -m "Jenkins Build" jenkins-build-$(date +%s)
         python3 -m unittest something_test.py
         ''')
     }
